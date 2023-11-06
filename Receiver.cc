61c78c547c075e3ee838ac28d70d0ec8122e4c3e
nashfisch <nash.fisch88@gmail.com>
2023-11-06 13:44:31 -0700

    Started Receiver.h and .cc

--- Receiver.cc
+++ Receiver.cc
@@ -2,7 +2,13 @@
 #include <vector>
 #include "Receiver.h"
 
+/*
 Receiver::Receiver(int max) : SlidingWindow(max){
     setMax(max);
     setWinSize(1);
-}
+}*/
+
+Receiver::Receiver(const std::string listenPort) {
+    lPort = listenPort;
+    Initialize();
+} 
--- Receiver.h
+++ Receiver.h
@@ -1,6 +1,14 @@
 #ifndef RECEIVER_H
 #define RECEIVER_H
 #include "SlidingWindow.h"
+#include <string>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <iostream>
+#include <netdb.h>
+
 
 
 class Receiver : public SlidingWindow {
@@ -9,12 +17,17 @@ class Receiver : public SlidingWindow {
      * @brief Constructor
      * @param max: max window size
     */
-    Receiver(int max);
-    
+    //Receiver(int max);
+    Receiver(const std::string& listenPort);
+
+    void Initalize();
+
+    void BounceBack(const std::string& message);
     
   private:
+  const std::string lPort;
     // Sequence number of the last in sequence frame received
-    int lastAck;
+    //int lastAck;
 };
 
 #endif
